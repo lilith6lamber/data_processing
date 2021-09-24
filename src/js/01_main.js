fcccaf210e5d0173560185519650293f79627312
Lilith Lamber <winter.rituel@gmail.com>
2021-09-24 12:45:53 +0300

    + loading screen

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,3 +1,26 @@
+const wait = (delay = 0) =>
+    new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (selector, visible) => {
+    selector = document.getElementById(selector);
+    if (visible) {
+        selector.classList.add("visible");
+        document.documentElement.style.overflowY = 'hidden';
+    } else {
+        selector.classList.remove("visible");
+        document.body.classList.add("visible");
+        document.documentElement.style.overflowY = 'visible';
+    }
+}
+
+setVisible('loadingScreen', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+    wait(1200).then(() => {
+        setVisible('loadingScreen', false);
+    }));
+
+    
 $(function () {
     AOS.init();
 
